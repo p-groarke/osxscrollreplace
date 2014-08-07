6d7b6aba031e3046827776b69c12c55465698650
Socapex <philippe_groarke@yahoo.ca>
2014-08-07 16:45:07 -0400

    Fix license copyright.

--- main.c
+++ main.c
@@ -1,10 +1,7 @@
-// Philippe Groarke, 2014
-
-
 /*          DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
                     Version 2, December 2004
 
- Copyright (C) 2004 Sam Hocevar <sam@hocevar.net>
+ Copyright (C) 2014 Philippe Groarke <philippe.groarke@gmail.com>
 
  Everyone is permitted to copy and distribute verbatim or modified
  copies of this license document, and changing it is allowed as long
